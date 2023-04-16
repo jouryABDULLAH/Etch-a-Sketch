54834e6f3d4adeb077e80525dee5ade4157f38d7
JouryABDULLAH <jours-222@outlook.com>
2023-04-16 04:33:50 -0700

    add fuctionality to  remove the grids

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 
 let container = document.querySelector('.box-container');
 let btn = document.querySelector('.btn');
+let box = document.querySelectorAll('.box');
 
 function make_grids(num = 16){ // default size is 16x16
+    console.log(num);
+    
+    // remove previous grids
+    container.innerHTML = '';
 
-    boxes = num* num ;
-    // console.log(num);
+    boxes = num * num ;
+
     for(let i=0 ; i<boxes ; i++){
         let box = document.createElement('div');
         box.classList.add('box');
+        console.log(num);
+
         container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
         container.appendChild(box);
@@ -21,10 +28,13 @@ make_grids();
 btn.addEventListener('click', () => {
 
     let size = 0;
-
+    console.log(size);
     do {
         size = prompt('Enter the size:','');
-    } while (size >= 100);
+        console.log(size);
+
+    } while (size > 100 );
+    console.log(size);
 
     make_grids(size);
 

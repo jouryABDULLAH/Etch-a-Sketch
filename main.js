e843b9f98a447b4195b0fe543c2f67ab07643930
JouryABDULLAH <jours-222@outlook.com>
2023-04-17 06:26:39 -0700

    add drawing functionality

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 
+//TODO: game mode, fill all the blanks before the timer ends
+// or connect tow grids without touching a grid twice in the path
 let container = document.querySelector('.box-container');
-let btn = document.querySelector('.btn');
-let box = document.querySelectorAll('.box');
+let create = document.getElementById('create');
+let reset = document.getElementById('reset');
+
+
+function draw(x){
+    
+    x.style.backgroundColor = 'black';
+    x.style.borderColor = 'black';
+  
+
+}
 
 function make_grids(num = 16){ // default size is 16x16
     console.log(num);
@@ -14,6 +25,7 @@ function make_grids(num = 16){ // default size is 16x16
     for(let i=0 ; i<boxes ; i++){
         let box = document.createElement('div');
         box.classList.add('box');
+        box.setAttribute("onmouseover","draw(this)");
         console.log(num);
 
         container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
@@ -25,7 +37,18 @@ function make_grids(num = 16){ // default size is 16x16
 
 make_grids();
 
-btn.addEventListener('click', () => {
+let box = document.querySelectorAll('.box');
+
+
+// let box = document.querySelectorAll('.box');
+
+// box.forEach( box => box.addEventListener('mouseover', (e) => {
+//     box.style.backgroundColor = 'black';
+//     box.style.borderColor = 'black';
+// }));
+
+
+create.addEventListener('click', () => {
 
     let size = 0;
     console.log(size);
@@ -38,4 +61,15 @@ btn.addEventListener('click', () => {
 
     make_grids(size);
 
+    
+});
+
+
+reset.addEventListener('click', () => {
+
+    let box = document.querySelectorAll('.box');
+    box.forEach( box => {
+        box.style.backgroundColor = 'white';
+        box.style.borderColor = 'rgb(182, 169, 172)';
+    });
 });
